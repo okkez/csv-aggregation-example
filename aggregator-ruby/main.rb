b6dedd19632573860954fcd42b0546079b7cacdf
okkez <okkez000@gmail.com>
2020-08-01 11:16:40 +0900

    Add aggregator ruby

---
+++ aggregator-ruby/main.rb
@@ -0,0 +1,11 @@
+require "csv"
+
+name_to_cost = Hash.new(0)
+
+CSV.foreach(ARGV[0], headers: true) do |row|
+  name_to_cost[row["name"]] += row["cost"].to_f
+end
+
+name_to_cost.sort_by {|k, _| k }.each do |name, cost|
+  puts "#{name}\t#{cost.floor(2)}"
+end
